bec56ff89932450eadd82315dd5153e5d695064b
MuNeNiCK <mune0127@munenick.me>
2022-10-11 15:17:16 +0900

    change

--- frontend/app/src/pages/_app.tsx
+++ frontend/app/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import Layout from '../../components/Layout';
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }) {
   return (
--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -1,13 +1,34 @@
-import Head from 'next/head'
- 
-export default function Home() {
-    return (
-        <div>
-            <Head>
-                <title>ホームページタイトル</title>
-                <meta name="description" content="ホームページ概要" />
-            </Head>
-            <h1>ホームページタイトル</h1>
-        </div>
-    )
+import fs  from 'fs';
+import matter from 'gray-matter';
+import PostCard from '../../components/PostCard';
+
+export const getStaticProps = () => {
+  const files = fs.readdirSync('posts');
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace(/\.md$/, '');
+    const fileContent = fs.readFileSync(`posts/${fileName}`, 'utf-8');
+    const { data } = matter(fileContent);
+    return {
+      frontMatter: data,
+      slug,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default function Home({ posts }) {
+  return (
+    <div className="my-8">
+      <div className="grid grid-cols-3">
+        {posts.map((post) => (
+          <PostCard key={post.slug} post={post} />
+        ))}
+      </div>
+    </div>
+  );
 }
